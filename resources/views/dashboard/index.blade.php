a0ecf9474d5754f16a8bb057bc209b0996647f40
Jemardematera <81813634+Jemardematera@users.noreply.github.com>
2021-05-12 22:21:25 +0800

    EDIT

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,36 +1,36 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container-fluid py-5 px-5 ">
+    <div class="container-fluid py-5 px-5 ">
 
-    <div class="mb-3">
-        <h1 class="display-4 title">Dashboard</h1>
-    </div>
+        <div class="mb-3">
+            <h1 class="display-4 title">Dashboard</h1>
+        </div>
 
-    <div class="row mb-5">
-        <div class="col-md-6 col-lg-3">
-            <div class="bg-success-200 text-white py-3 px-4 w-100">
-                <div class="d-flex justify-content-between">
-                    <div class="icon">
-                        <p style="font-size: 4rem;" class="mb-0"><i class="fa fa-graduation-cap"></i></p>
-                    </div>
-                    <div class="text-right">
-                        <p class="mb-0" style="font-size: 3rem;">{{ $student_count }}</p>
-                        <span class="">Enrolled Students</span>
+        <div class="row mb-5">
+            <div class="col-md-4 col">
+                <div class="bg-success-200 text-white py-3 px-4 w-100">
+                    <div class="d-flex justify-content-between">
+                        <div class="icon">
+                            <p style="font-size: 4rem;" class="mb-0"><i class="fa fa-graduation-cap"></i></p>
+                        </div>
+                        <div class="text-right">
+                            <p class="mb-0" style="font-size: 3rem;">{{ $student_count }}</p>
+                            <span class="">Enrolled Students</span>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div class="bg-success-100 text-white px-4 py-1">
-                <a href="{{ route('students.index') }}" style="color: white;">
-                    <div class="d-flex justify-content-between align-items-center">
+                <div class="bg-success-100 text-white px-4 py-1">
+                    <a href="{{ route('students.index') }}" style="color: white;">
+                        <div class="d-flex justify-content-between align-items-center">
                             <p class="mb-0" style="font-size: 0.7rem;">VIEW MORE</p>
                             <p class="mb-0"><i class="fa fa-arrow-circle-right"></i></p>
-                    </div>
-                 </a>
+                        </div>
+                    </a>
+                </div>
             </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-            <div class="bg-info-200 text-white py-3 px-4 w-100">
+            <div class="col-md-4 col"">
+                        <div class=" bg-info-200 text-white py-3 px-4 w-100">
                 <div class="d-flex justify-content-between">
                     <div class="icon">
                         <p style="font-size: 4rem;" class="mb-0"><i class="fa fa-users"></i></p>
@@ -44,54 +44,13 @@
             <div class="bg-info-100 text-white px-4 py-1">
                 <a href="{{ route('employees.index') }}" style="color: white;">
                     <div class="d-flex justify-content-between align-items-center">
-                            <p class="mb-0" style="font-size: 0.7rem;">VIEW MORE</p>
-                            <p class="mb-0"><i class="fa fa-arrow-circle-right"></i></p>
+                        <p class="mb-0" style="font-size: 0.7rem;">VIEW MORE</p>
+                        <p class="mb-0"><i class="fa fa-arrow-circle-right"></i></p>
                     </div>
-                 </a>
+                </a>
             </div>
         </div>
-        <div class="col-md-6 col-lg-3">
-            <div class="bg-warning-100 text-white py-3 px-4 w-100">
-                <div class="d-flex justify-content-between">
-                    <div class="icon">
-                        <p style="font-size: 4rem;" class="mb-0"><i class="fa fa-id-card-o"></i></p>
-                    </div>
-                    <div class="text-right">
-                        <p class="mb-0" style="font-size: 3rem;">{{ $section_count }}</p>
-                        <span class="">Sections</span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-warning-200 text-white px-4 py-1">
-                <a href="{{ route('sections.index') }}" style="color: white;">
-                    <div class="d-flex justify-content-between align-items-center">
-                            <p class="mb-0" style="font-size: 0.7rem;">VIEW MORE</p>
-                            <p class="mb-0"><i class="fa fa-arrow-circle-right"></i></p>
-                    </div>
-                 </a>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-3">
-            <div class="bg-violet-100 text-white py-3 px-4 w-100">
-                <div class="d-flex justify-content-between">
-                    <div class="icon">
-                        <p style="font-size: 4rem;" class="mb-0"><i class="fa fa-pencil"></i></p>
-                    </div>
-                    <div class="text-right">
-                        <p class="mb-0" style="font-size: 3rem;">{{ $class_count }}</p>
-                        <span class="">Classes</span>
-                    </div>
-                </div>
-            </div>
-            <div class="bg-violet-200 text-white px-4 py-1">
-                <a href="{{ route('schoolClass.index') }}" style="color: white;">
-                    <div class="d-flex justify-content-between align-items-center">
-                            <p class="mb-0" style="font-size: 0.7rem;">VIEW MORE</p>
-                            <p class="mb-0"><i class="fa fa-arrow-circle-right"></i></p>
-                    </div>
-                 </a>
-            </div>
-        </div>
+
     </div>
 
     {{-- <div class="row mb-5">
@@ -106,37 +65,41 @@
 
     <div class="row mb-5">
         <div class="col-md-8">
-        <div class="card" style="height: 500px;">
+            <div class="card" style="height: 500px;">
                 <div class="card-body">
-                     <p style="color: black; font-size: 1.5rem;">Department Heads</p>
-                     <div class="border mb-3"></div>
-                     @foreach ($departments as $department)
+                    <p style="color: black; font-size: 1.5rem;">Department Heads</p>
+                    <div class="border mb-3"></div>
+                    @foreach ($departments as $department)
                         <div class="d-flex align-items-center mb-3">
-                            <div class="portrait-sm mr-4" style="background-color: {{ $department->role->assigned_officer->color }};">
-                                <p class="default-font my-0">{{ $department->role->assigned_officer->two_initials }}</p>
+                            <div class="portrait-sm mr-4"
+                                style="background-color: {{ $department->role->assigned_officer->color }};">
+                                <p class="default-font my-0">{{ $department->role->assigned_officer->two_initials }}
+                                </p>
                             </div>
                             <div class="d-flex align-items-center">
                                 <div>
-                                    <p class="mb-0" style="font-size: 1.2rem;">{{ $department->role->assigned_officer['full_name'] }}</span>
-                                    <p class="mb-0 text-secondary" style="font-size: 0.8rem; ">{{ $department->role->role_name }}</span>
+                                    <p class="mb-0" style="font-size: 1.2rem;">
+                                        {{ $department->role->assigned_officer['full_name'] }}</span>
+                                    <p class="mb-0 text-secondary" style="font-size: 0.8rem; ">
+                                        {{ $department->role->role_name }}</span>
                                 </div>
                             </div>
                         </div>
-                     @endforeach
+                    @endforeach
                 </div>
             </div>
         </div>
-        
+
         <div class="col-md-4">
             <div class="card" style="height: 500px;">
                 <div class="card-body">
-                     <canvas id="by_strand_chart" width="200" height="200"></canvas>
+                    <canvas id="by_strand_chart" width="200" height="200"></canvas>
                 </div>
             </div>
         </div>
-        
+
     </div>
-</div>
+    </div>
 @endsection
 
 @section('custom-meta')
@@ -146,55 +109,58 @@
 @endsection
 
 @section('custom-css')
-<style>
-    .bg-success-100{
-        background-color: #17A769;
-    }
+    <style>
+        .bg-success-100 {
+            background-color: #17A769;
+        }
 
-    .bg-success-200{
-        background-color: #28B779;
-    }
+        .bg-success-200 {
+            background-color: #28B779;
+        }
 
-    .bg-warning-100{
-        background-color: #FFB84A;
-    }
-    .bg-warning-200{
-        background-color: #CA943D;
-    }
-    .bg-violet-100{
-        background-color: #852C9A;
-    }
+        .bg-warning-100 {
+            background-color: #FFB84A;
+        }
 
-    .bg-violet-200{
-        background-color: #751E87;
-    }
+        .bg-warning-200 {
+            background-color: #CA943D;
+        }
 
-    .bg-info-100{
-        background-color: #2395C9; 
-    }
-    .bg-info-200{
-        background-color: #27A9E3; 
-    }
-</style>
+        .bg-violet-100 {
+            background-color: #852C9A;
+        }
+
+        .bg-violet-200 {
+            background-color: #751E87;
+        }
+
+        .bg-info-100 {
+            background-color: #2395C9;
+        }
+
+        .bg-info-200 {
+            background-color: #27A9E3;
+        }
+
+    </style>
 @endsection
 
 @section('scripts')
     <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
     <script>
-
         var strand_chart = document.getElementById('by_strand_chart').getContext('2d');
-        var strand_chart_data =  JSON.parse($('meta[name="students_by_strand_data"]').attr('content'));
+        var strand_chart_data = JSON.parse($('meta[name="students_by_strand_data"]').attr('content'));
         var strand_pie_chart = new Chart(strand_chart, {
             type: 'doughnut',
             data: {
                 labels: strand_chart_data[0],
                 datasets: [{
-                label: '# of students per grade level',
-                data: strand_chart_data[1],
-                backgroundColor: strand_chart_data[2],
-                borderWidth: 0
+                    label: '# of students per grade level',
+                    data: strand_chart_data[1],
+                    backgroundColor: strand_chart_data[2],
+                    borderWidth: 0
                 }],
-                
+
             },
             options: {
                 title: {
@@ -212,7 +178,7 @@
 
 
         var daily_clearance_ctx = document.getElementById("daily_clearance").getContext('2d');
-        var daily_clearance_data =  JSON.parse($('meta[name="daily_clearance_data"]').attr('content'));
+        var daily_clearance_data = JSON.parse($('meta[name="daily_clearance_data"]').attr('content'));
         var daily_clearance_chart = new Chart(daily_clearance_ctx, {
             type: 'line',
             data: {
@@ -224,17 +190,18 @@
                     borderColor: '#2196f3', // Add custom color border (Line)
                     backgroundColor: '#2196f3', // Add custom color background (Points and Fill)
                     borderWidth: 3 // Specify bar border width
-                },{
+                }, {
                     label: 'Students complied (Department Requirement)', // Name the series
                     data: daily_clearance_data['department_clearance'], // Specify the data values array
                     fill: false,
                     borderColor: '#D31E1E', // Add custom color border (Line)
                     backgroundColor: '#D31E1E', // Add custom color background (Points and Fill)
                     borderWidth: 3 // Specify bar border width
-                }]},
+                }]
+            },
             options: {
-            responsive: true, // Instruct chart js to respond nicely.
-            maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
+                responsive: true, // Instruct chart js to respond nicely.
+                maintainAspectRatio: false, // Add to prevent default behaviour of full-width/height 
             },
             options: {
                 title: {
@@ -245,5 +212,6 @@
                 },
             }
         });
+
     </script>
 @endsection
