797ff9c85ec71edc1565133176e320e1e113d2ed
Jemardematera <81813634+Jemardematera@users.noreply.github.com>
2021-05-19 13:19:53 +0800

    landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -134,7 +134,12 @@
                 <b>
                     <div class="mt-5" style="margin-left:550px;">
                         @auth
-                            <a href="{{ url('/home') }}">Home</a>
+                            <div class="mt-5" style="margin-left: 100px;">
+                                <a href="{{ url('/home') }}" class="btn btn-outline-dark   "
+                                    style="font-famiy: Rounded MT Bold; font-size:20px; color: gray"> Home
+                                </a>
+
+                            </div>
                         @else
 
                             <a href="{{ route('login') }}" class="btn btn-outline-dark   "
@@ -343,8 +348,102 @@
         made this process so easy and convenient.”
     </p>
 
+    <p class="default-font display-1 mb-5"
+        style=" font-size:30px;margin-top: 0px;margin-left: 500px; margin-right: 200px;family-font:arial">
+        Ready to start getting better results?
+    </p>
 
+    <a href="{{ route('login') }}" class="btn btn-outline-dark "
+        style="font-famiy: Rounded MT Bold; font-size:20px;margin-left: 650px;  color:gray">Go started
+    </a>
 
+    ...
+    <style>
+        .owl-carousel .item {
+            height: 18rem;
+            background: white;
+            padding: 1rem;
+        }
+
+        .owl-carousel .item h4 {
+            color: #FFF;
+            font-weight: 400;
+            margin-top: 0rem;
+        }
+
+    </style>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"></script>
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+    <link rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.green.min.css" />
+    <script>
+        jQuery(document).ready(function($) {
+            $('.owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+                nav: true,
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    600: {
+                        items: 3
+                    },
+                    1000: {
+                        items: 5
+                    }
+                }
+            })
+        })
+
+    </script>
+    ...
+
+    <div class="owl-carousel owl-theme mt-5">
+        <div class="item">
+            <h4><img src="/img/set1.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set2.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set3.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set4.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set5.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set6.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set7.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set8.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set9.jpg" alt="" style="width:250px;"></h4>
+        </div>
+        <div class="item">
+            <h4><img src="/img/set10.jpg" alt="" style="width:250px;"></h4>
+        </div>
+
+    </div>
+    <div cl <div class="card text-center mt-5">
+
+        <div class="card-body">
+
+        </div>
+        <div class="card-footer text-muted">
+            © 2021 GardnerCollege.ph
+            | Terms and Conditions | Privacy Policy | Sitemap
+        </div>
+    </div>
 
 
 </body>
