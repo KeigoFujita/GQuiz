d82565bf5786086e3319ec0531cfaed6bd775d7f
Jemardematera <81813634+Jemardematera@users.noreply.github.com>
2021-05-30 23:05:12 +0800

    teacher

--- resources/views/teacher/quizzes/items/result-set.blade.php
+++
@@ -1,7 +0,0 @@
-<ul class="list-group">
-    @forelse($results as $result)
-        <li class="list-group-item" style="border: 1px solid lightgray !important; cursor: pointer !important;" id="result-item" onclick='setItemDefinition("{{ $result }}")'>{{ $result }}</li>
-    @empty
-        <li class="list-group-item border-bottom"> No results found. </li>
-    @endforelse
-</ul>
